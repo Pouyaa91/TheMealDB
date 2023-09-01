50f04d3e35efb8c0349902a1b39d484958c5c3b4
Pouya Mazloumpour <mazloumpour.pouya@gmail.com>
2023-09-01 14:20:51 -0700

    feat: add model module and category model

---
+++ core/model/build.gradle.kts
@@ -0,0 +1,3 @@
+plugins {
+    id("themealdb.jvm.library")
+}
---
+++ core/model/src/main/java/com/pouyaa/model/Category.kt
@@ -0,0 +1,8 @@
+package com.pouyaa.model
+
+data class Category(
+    private val id: String,
+    private val name: String,
+    private val imageUrl: String,
+    private val description: String
+)
