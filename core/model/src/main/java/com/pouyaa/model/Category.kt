4e74417fa0cb2b694f5e73c6a71cbac9989e0df0
Pouya Mazloumpour <mazloumpour.pouya@gmail.com>
2023-09-01 15:16:30 -0700

    fix: fix category private visibility

--- core/model/src/main/java/com/pouyaa/model/Category.kt
+++ core/model/src/main/java/com/pouyaa/model/Category.kt
@@ -1,8 +1,8 @@
 package com.pouyaa.model
 
 data class Category(
-    private val id: String,
-    private val name: String,
-    private val imageUrl: String,
-    private val description: String
+    val id: String,
+    val name: String,
+    val imageUrl: String,
+    val description: String
 )
